321ca63238dc680f84e2777a43267f00163f58fe
Aftab Faisal <aftab.22.1996@gmail.com>
2020-09-22 23:22:02 +0530

    completed cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import Button from "../buttons/button.component";
+import CartItem from "../cart-item/cart-item.component";
+import { connect } from "react-redux";
 import "./cart-dropdown.styles.scss";
 
-function Cart() {
+function Cart({ cartItems }) {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
 }
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(Cart);
