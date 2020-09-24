b27d6cf1546b796f1bcc376356b241aae9350481
Aftab Faisal <aftab.22.1996@gmail.com>
2020-09-24 23:51:25 +0530

    adding products json to global state and adding selector

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,6 +5,7 @@ import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import productMenuReducer from "./products-menu/products.reducer";
 
 const persistConfig = {
   key: "root",
@@ -15,6 +16,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  products: productMenuReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
